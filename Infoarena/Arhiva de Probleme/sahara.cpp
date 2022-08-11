2ab23dc73df8925b3f36078150f869f3544a101e
mircea1711 <maiereanmircea@cnprbeclean.ro>
2022-08-11 15:16:55 +0300

    First Page of Infoarena

--- Infoarena/Arhiva de Probleme/sahara.cpp
+++ Infoarena/Arhiva de Probleme/sahara.cpp
@@ -1,27 +1,27 @@
 #include <fstream>
 #include <queue>
 using namespace std;
- 
+
 ifstream fin("sahara.in");
 ofstream fout("sahara.out");
- 
-int dx[] = {-1, 1, 0 ,0};
-int dy[] = {0, 0, -1 ,1};
- 
+
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+
 int n, m, cnt, x1, x2, y1, y2, maxi, q, Q, t;
 int64_t dp[1005][1005];
- 
-queue < pair <int, int > > coada;
-queue < pair <int, int > > crt;
- 
+
+queue<pair<int, int>> coada;
+queue<pair<int, int>> crt;
+
 inline bool valid(int x, int y)
 {
     return x && y && x <= n && y <= m && dp[x][y] >= Q;
 }
- 
+
 void Lee()
 {
- 
+
     while (!crt.empty())
     {
         int x = crt.front().first, y = crt.front().second;
@@ -39,12 +39,12 @@ void Lee()
         }
     }
 }
- 
+
 int main()
 {
     fin >> n >> m >> Q >> t;
- 
-    while(t--)
+
+    while (t--)
     {
         fin >> x1 >> y1 >> x2 >> y2 >> q;
         dp[x1][y1] += q;
@@ -52,14 +52,15 @@ int main()
         dp[x1][y2 + 1] -= q;
         dp[x2 + 1][y1] -= q;
     }
- 
+
     for (int i = 1; i <= n; ++i)
         for (int j = 1; j <= m; ++j)
         {
             dp[i][j] += dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1];
-            if (dp[i][j] >= Q) coada.push(make_pair(i, j));
+            if (dp[i][j] >= Q)
+                coada.push(make_pair(i, j));
         }
- 
+
     while (!coada.empty())
     {
         int x = coada.front().first, y = coada.front().second;
@@ -69,12 +70,13 @@ int main()
             crt.push(make_pair(x, y));
             cnt = 1;
             Lee();
-            if (cnt > maxi) maxi = cnt;
+            if (cnt > maxi)
+                maxi = cnt;
         }
         coada.pop();
     }
- 
+
     fout << maxi << "\n";
- 
+
     return 0;
 }
