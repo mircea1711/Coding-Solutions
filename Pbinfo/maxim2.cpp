10fa7bf937affde47a535f50d44c871ee59a43d4
mircea1711 <maiereanmircea@cnprbeclean.ro>
2022-08-21 18:48:39 +0300

    Added #1-#100 from Pbinfo

---
+++ Pbinfo/maxim2.cpp
@@ -0,0 +1,12 @@
+#include <fstream>
+using namespace std;
+ifstream cin("maxim.in");
+ofstream cout("maxim.out");
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    if(a>b) swap(a,b);
+    cout<<b;
+    return 0;
+}
---
+++ Pbinfo/numaruldecifre.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,k=1;
+    cin>>n;
+    while(n>=10)
+    {k++;
+    n=n/10;}
+    cout<<k;
+    return 0;
+}
